ad4be383846985668ab104592757a7407d9d0a46
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-26 23:31:28 +0200

    Use common TodoMVC app styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
+import './index.css';
+import 'todomvc-app-css/index.css';
+import 'todomvc-common/base.css';
+
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
