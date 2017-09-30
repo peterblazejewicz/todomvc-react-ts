f97d62be2fc7ca7de2711b181832572d4e7ca652
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-30 22:37:09 +0200

    Update filter type to TS 2.4 enum

--- src/constants/filtertype.ts
+++ src/constants/filtertype.ts
@@ -1,9 +1,10 @@
-export const SHOW_ACTIVE = 'show_active';
-export const SHOW_ALL = 'show_all';
-export const SHOW_COMPLETED = 'show_completed';
-
-export type SHOW_ACTIVE = typeof SHOW_ACTIVE;
-export type SHOW_ALL = typeof SHOW_ALL;
-export type SHOW_COMPLETED = typeof SHOW_COMPLETED;
-
-export type FilterType = SHOW_ACTIVE | SHOW_ALL | SHOW_COMPLETED;
+/**
+ * Filter types as typed enum
+ * @export
+ * @enum {string}
+ */
+export enum FilterType {
+  SHOW_ACTIVE = 'show_active',
+  SHOW_ALL = 'show_all',
+  SHOW_COMPLETED = 'show_completed',
+}
